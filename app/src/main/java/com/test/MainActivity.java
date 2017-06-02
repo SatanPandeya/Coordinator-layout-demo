0c697e658c7d9774931cca57afd0b8a85d97f8cd
SatanPandeya <satanpandeya@gmail.com>
2017-06-02 11:22:12 +0545

    test layout weight property

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -1,27 +1,50 @@
 package com.test;
 
+import android.content.Intent;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
+    @BindView(R.id.toolbarId)
+    Toolbar toolbar;
+    @BindView(R.id.fabButtonId)
+    FloatingActionButton floatingActionButton;
     private Drawer drawer;
+    private Unbinder unbinder;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbarId);
+        unbinder = ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         drawer = new DrawerBuilder().withActivity(this).withToolbar(toolbar).build();
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         drawer.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
+    }
+
+    @OnClick(R.id.fabButtonId)
+    public void navigateView(View view){
+        startActivity(new Intent(MainActivity.this, TestActivity.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
---
+++ app/src/main/java/com/test/TestActivity.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wolfmatrix on 6/2/17.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
